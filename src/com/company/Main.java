8e15b8ee111f0cc84c5901f99bec481765bd2dd3
Anish-1703 <71100656+Anish-1703@users.noreply.github.com>
2020-11-12 12:08:55 -0800

    Initial code

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        // Following is the code where a Cat greets the Students of CS 151
+
+        System.out.println("   /\\_/\\     ------ ");
+        System.out.println("  / o o \\  / Welcome \\");
+        System.out.println(" (   \"   ) <   To    |");
+        System.out.println("  \\~(*)~/  \\ GitHub! /");
+        System.out.println("   // \\\\     ------ ");
+
+    }
+}
