f0a99f6c7fb9553e309d04d3852193a292481b5a
Anish-1703 <71100656+Anish-1703@users.noreply.github.com>
2020-11-12 12:46:13 -0800

    Trying pull requests

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Following is the code where a Cat greets the Students of CS 151
+
+        System.out.println("   /\\_/\\     ------ ");
+        System.out.println("  / o o \\  / Welcome \\");
+        System.out.println(" (   \"   ) <   To    |");
+        System.out.println("  \\~(*)~/  \\ CS151! /");
+        System.out.println("   // \\\\     ------ ");
+
+
+    }
+    
+}
